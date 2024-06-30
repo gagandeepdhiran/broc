38ab2573e7e1808dd48d37422b8fd98d8697db5a
gagandeep-mapout <gagandeep.singh@dreampool.in>
2024-06-30 23:20:40 +0530

    Changed chunk size

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
+  build: { chunkSizeWarningLimit: 1600, },
   base: ((process.env.GITHUB_REPOSITORY ?? "") + "/").match(/(\/.*)/)?.[1],
 });
